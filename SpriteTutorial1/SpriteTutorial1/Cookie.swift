229e89d2be13469f0ae6477ddb4355a64d90e664
josmoss <josmoss@gmail.com>
2016-09-02 11:49:18 -0600

    Created the Cookie Class. Added an enum for CookieType.

---
+++ SpriteTutorial1/SpriteTutorial1/Cookie.swift
@@ -0,0 +1,46 @@
+//
+//  Cookie.swift
+//  SpriteTutorial1
+//
+//  Created by Joe Moss on 9/2/16.
+//  Copyright © 2016 TouchType LLC. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+        "Croissant",
+        "Cupcake",
+        "Danish",
+        "Donut",
+        "Macaroon",
+        "SugarCookie"]
+        
+    return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!
+    }
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
