7e0a502c87785c2e3388d1706843a408ca4a7a64
josmoss <josmoss@gmail.com>
2016-09-05 22:32:06 -0600

    Created additional properties for the GameScene. Added new cookiesLayer and gameLayer. Added new method (addSpritesForCookies) and helper method (pointForColumn) for adding sprites to the scene.

--- SpriteTutorial1/SpriteTutorial1/Cookie.swift
+++ SpriteTutorial1/SpriteTutorial1/Cookie.swift
@@ -36,13 +36,19 @@ enum CookieType: Int, CustomStringConvertible {
     }
 }
 
-class Cookie: CustomStringConvertible {
+class Cookie: CustomStringConvertible, Hashable {
+    
     var column: Int
     var row: Int
     let cookieType: CookieType
     var sprite: SKSpriteNode?
+    
     var description: String {
         return "type:\(cookieType) square:(\(column),\(row))"
+    }
+    
+    var hashValue: Int {
+        return row*10 + column
     }
     
     init(column: Int, row: Int, cookieType: CookieType) {
@@ -50,4 +56,9 @@ class Cookie: CustomStringConvertible {
         self.row = row
         self.cookieType = cookieType
     }
+    
+}
+
+func ==(lhs: Cookie, rhs: Cookie) -> Bool {
+    return lhs.column == rhs.column && lhs.row == rhs.row
 }
---
+++ SpriteTutorial1/SpriteTutorial1/Level.swift
@@ -0,0 +1,44 @@
+//
+//  Level.swift
+//  SpriteTutorial1
+//
+//  Created by Joe Moss on 9/2/16.
+//  Copyright © 2016 TouchType LLC. All rights reserved.
+//
+
+import Foundation
+
+let NumColumns = 9
+let NumRows = 9
+
+class Level {
+    private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+    
+    func cookieAtColumn(column: Int, row: Int) -> Cookie? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return cookies[column, row]
+    }
+    
+    func shuffle() -> Set<Cookie> {
+        return createInitialCookies()
+    }
+    
+    private func createInitialCookies() -> Set<Cookie> {
+        var set = Set<Cookie>()
+        
+        for row in 0..<NumRows {
+            for column in 0..<NumColumns {
+                
+                var cookieType = CookieType.random()
+                
+                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                cookies[column, row] = cookie
+                
+                set.insert(cookie)
+            }
+        }
+        return set
+    }
+}
+
