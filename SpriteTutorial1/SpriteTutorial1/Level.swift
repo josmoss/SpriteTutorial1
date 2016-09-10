3fd6f799bb3d122ba508d2772430ebc6d704cf27
josmoss <josmoss@gmail.com>
2016-09-09 20:07:48 -0600

    Revised random cookie generator with logic that randomizes and also makes sure a chain of 3 or more is not created.

--- SpriteTutorial1/SpriteTutorial1/Level.swift
+++ SpriteTutorial1/SpriteTutorial1/Level.swift
@@ -41,7 +41,15 @@ class Level {
                 
                 if tiles[column, row] != nil {
                 
-                    var cookieType = CookieType.random()
+                    var cookieType: CookieType
+                    repeat {
+                        cookieType = CookieType.random()
+                    } while (column >= 2 &&
+                            cookies[column - 1, row]?.cookieType == cookieType &&
+                            cookies[column - 2, row]?.cookieType == cookieType)
+                        || (row >= 2 &&
+                            cookies[column, row - 1]?.cookieType == cookieType &&
+                            cookies[column, row - 2]?.cookieType == cookieType)
                     
                     let cookie = Cookie(column: column, row: row, cookieType: cookieType)
                     cookies[column, row] = cookie
