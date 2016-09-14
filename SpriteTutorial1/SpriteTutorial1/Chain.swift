27fc615e78e74b6ed777d49d6bb832e42fbf5b0a
josmoss <josmoss@gmail.com>
2016-09-14 15:14:22 -0600

    Created the Chain class for finding chains of 3 or more cookies of the same type.

---
+++ SpriteTutorial1/SpriteTutorial1/Chain.swift
@@ -0,0 +1,59 @@
+//
+//  Chain.swift
+//  SpriteTutorial1
+//
+//  Created by Joe Moss on 9/14/16.
+//  Copyright © 2016 TouchType LLC. All rights reserved.
+//
+
+import Foundation
+
+class Chain: Hashable, CustomStringConvertible {
+    var cookies = [Cookie]()
+    
+    enum ChainType: CustomStringConvertible {
+        case Horizontal
+        case Vertical
+        
+        var description: String {
+            switch self {
+            case .Horizontal: return "Horizontal"
+            case .Vertical: return "Vertical"
+            }
+        }
+    }
+    
+    var chainType: ChainType
+    
+    init(chainType: ChainType) {
+        self.chainType = chainType
+    }
+    
+    func addCookie(cookie: Cookie) {
+        cookies.append(cookie)
+    }
+    
+    func firstCookie() -> Cookie {
+        return cookies[0]
+    }
+    
+    func lastCookie() -> Cookie {
+        return cookies[cookies.count - 1]
+    }
+    
+    var length: Int {
+        return cookies.count
+    }
+    
+    var description: String {
+        return "type:\(chainType) cookies:\(cookies)"
+    }
+    
+    var hashValue: Int {
+        return cookies.reduce (0) { $0.hashValue ^ $1.hashValue }
+    }
+}
+
+func ==(lhs: Chain, rhs: Chain) -> Bool {
+    return lhs.cookies == rhs.cookies
+}
